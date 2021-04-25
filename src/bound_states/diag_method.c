532dca8106013be0533d07d6831ad1cfc8e55cfd
elenaacinapura <elena.acinapura@gmail.com>
2021-04-25 19:12:58 +0200

    viene tutto ora siii

--- src/bound_states/diag_method.c
+++ src/bound_states/diag_method.c
@@ -25,67 +25,91 @@ int main() {
 	Params_lj p_lj;
 
 	if (POT == 0) {
+		printf("------------------------------------------------\nDIAGONALIZATION METHOD - COSH POTENTIAL \nSearching bound energies for xi = %.3lf\n\n", xi);
+
 		xi = 0.01;
 		a = 2.0 / xi;
 		p_cosh.a = a;
 
 		dx = 2.0 * R / (N - 1);
 
+		double diag[N];
+		double subdiag[N - 1];
+		double res_eigval[N];
+		double res_eigvec[N][N];
+
+		/* Fill diag and subdiag */
+		for (int i = 0; i < N; i++) {
+			double x = -R + i * dx;
+			diag[i] = V_cosh(x, &p_cosh) + xi / (dx * dx);
+			if (i < N - 1) {
+				subdiag[i] = -xi / (2.0 * dx * dx);
+			}
+		}
+
+		int info = diagonalize_tridiag_double(N, diag, subdiag, (double*)res_eigvec, res_eigval);
+		assert(info == 0);
+
+		int cnt_bound = 0;
+		for (int i = 0; i < N; i++) {
+			if (res_eigval[i] > 0.0) {
+				break;
+			}
+			printf("E_%d = %g\n", i, res_eigval[i]);
+			cnt_bound++;
+		}
+		printf("\nNumber of bound states found: %d\n", cnt_bound);
+		printf("------------------------------------------------\n");
+
 	} else if (POT == 1) {
+		printf("------------------------------------------------\nDIAGONALIZATION METHOD - LENNARD JONES POTENTIAL\nSearching bound energies for l = %d\n\n", l);
+
 		double kB = 1.38e-23;
 		double eps = 35.7 * kB;
 		double s = 2.79e-10;
 		double hbar = 1.05e-34;
-		double m = 20 * 1.66e-27;
-		printf("Value of l: ");
-		scanf("%d", &l);
+		double m = 10 * 1.66e-27;
 
 		xi = pow(hbar, 2) / (m * s * s * eps);
-		a = 2.0/xi;
+		a = 2.0 / xi;
 
-		double x0 = 0.4;
-		dx = (R - x0) / ((double)(N - 1));
+		double r0 = 0.4;
+		dx = (R - r0) / ((double)(N - 1));
 
-		p_lj.l = l;
 		p_lj.a = a;
-	}
 
-	double diag[N];
-	double subdiag[N - 1];
-	double res_eigval[N];
-	double res_eigvec[N][N];
+		int cnt_bound = 0;
 
-	/* Fill diag and subdiag */
-	for (int i = 0; i < N; i++) {
-		double x = -R + i * dx;
-		if (POT == 0) {
-			diag[i] = V_cosh(x, &p_cosh) + xi / (dx * dx);
-		}
-		if (POT == 1) {
-			diag[i] = V_lj(x, &p_lj) + xi / (dx * dx);
-		}
-		if (i < N - 1) {
-			subdiag[i] = -xi / (2.0 * dx * dx);
-		}
-	}
+		for (int l = 0; l < 10; l++) {
+			p_lj.l = l;
 
-	int info = diagonalize_tridiag_double(N, diag, subdiag, (double*)res_eigvec, res_eigval);
-	assert(info == 0);
+			double diag[N];
+			double subdiag[N - 1];
+			double res_eigval[N];
+			double res_eigvec[N][N];
 
-	if (POT == 0) {
-		printf("------------------------------------------------\nDIAGONALIZATION METHOD - COSH POTENTIAL \nSearching bound energies for xi = %.3lf\n\n", xi);
-	}
-	if (POT == 1) {
-		printf("------------------------------------------------\nDIAGONALIZATION METHOD - LENNARD JONES POTENTIAL\nSearching bound energies for l = %d\n\n", l);
-	}
-	int cnt_bound = 0;
-	for (int i = 0; i < N; i++) {
-		if (res_eigval[i] > 0.0) {
-			break;
+			/* Fill diag and subdiag */
+			for (int i = 0; i < N; i++) {
+				double x = r0 + i * dx;
+				diag[i] = V_lj(x, &p_lj) + xi / (dx * dx);
+				if (i < N - 1) {
+					subdiag[i] = -xi / (2.0 * dx * dx);
+				}
+			}
+
+			int info = diagonalize_tridiag_double(N, diag, subdiag, (double*)res_eigvec, res_eigval);
+			assert(info == 0);
+
+			printf("l = %d\n", l);
+			for (int i = 0; i < N; i++) {
+				if (res_eigval[i] > 0.0) {
+					break;
+				}
+				printf("E_%d = %g\n", i, res_eigval[i]);
+				cnt_bound++;
+			}
 		}
-		printf("E_%d = %g\n", i, res_eigval[i]);
-		cnt_bound++;
+		printf("\nNumber of bound states found: %d\n", cnt_bound);
+		printf("------------------------------------------------\n");
 	}
-	printf("\nNumber of bound states found: %d\n", cnt_bound);
-	printf("------------------------------------------------\n");
 }
